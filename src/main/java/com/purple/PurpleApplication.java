e251ec9388789400a9be78ff483053eb00a4741f
SJS <jinsoo1932@gmail.com>
2023-04-14 17:34:31 +0900

    4번 진행중

--- src/main/java/com/purple/PurpleApplication.java
+++ src/main/java/com/purple/PurpleApplication.java
@@ -2,7 +2,7 @@ package com.purple;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.nio.charset.StandardCharsets;
+import java.io.UnsupportedEncodingException;
 
 import static java.lang.Integer.parseInt;
 
@@ -13,6 +13,7 @@ public class PurpleApplication {
         long startTime = System.currentTimeMillis();
 //        SpringApplication.run(PurpleApplication.class, args);
 
+        ///////////////////////////////////////////////////////////////////////////////////////
         // 1.
 //        for(int i = -20; i<20; i++){
 //            int result = quarterCalculator.quarterCalculator(i);
@@ -24,6 +25,7 @@ public class PurpleApplication {
 //            }
 //        }
 
+        ///////////////////////////////////////////////////////////////////////////////////////
         // 2. 미완 >> quick Sort, random pivot
 ////        int[] nums = { 0, 1, 2, 6, 9, 11, 15 }; // 3
 ////        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14 }; // 10
@@ -36,7 +38,7 @@ public class PurpleApplication {
 ////        quickSort(nums, 0, nums.length - 1);
 ////        System.out.println("After sorting: " + Arrays.toString(nums));
 
-
+        ///////////////////////////////////////////////////////////////////////////////////////
         // 3.
 ////        String[] scoreList = {"1:1", "2:2", "3:3", "4:4", "1:1", "2:2", "3:3", "4:4", "1:1", "2:2"}; // 10
 ////        String[] scoreList = {"4:1", "4:2", "4:3", "4:3", "4:1", "4:2", "4:3", "4:3", "4:1", "4:2"}; // 30
@@ -46,37 +48,25 @@ public class PurpleApplication {
 //
 //        System.out.println("gameScore : " +gameScore.gameScore(scoreList));
 
-        // 4. EUC-KR를 통한 2바이트 처리 및 개행문자 삽입 필요
-//        String text = "이 글은 도커에 대해 1도 모르는 시스템 관리자나 서버 개발자를 대상으로 도커 전반에 대해 얕고 넓은 지식을 담고 있습니다. 도커가 등장한 배경과 도커의 역사, 그리고 도커의 핵심 개념인 컨테이너와 이미지에 대해 알아보고 실제로 도커를 설치하고 컨테이너를 실행해 보도록 하겠습니다.";
-        String text = "이 글은 도커에 대해 1도 모르는 시스템 관리자나 서버 개발자를 대상으로 도커 전반에 대해 얕고 넓은 지식을 담고 있습니다. 도커가 등장한 배경과";
-        autoNewlineProc(text);
 
-//        String test = "체";
-//        byte[] test2 = test.getBytes();
-//        System.out.println("test2 = " + test2.length);
-//        System.out.println("test2 = " + test2.toString());
 
-        String test3 = "a";
-        String test4 = "한";
+        ///////////////////////////////////////////////////////////////////////////////////////
+        // 4. 한글 체크, 공백 체크 필요
+        String text = "이 글은 도커에 대해 1도 모르는 시스템 관리자나 서버 개발자를 대상으로 도커 전반에 대해 얕고 넓은 지식을 담고 있습니다. 도커가 등장한 배경과 도커의 역사, 그리고 도커의 핵심 개념인 컨테이너와 이미지에 대해 알아보고 실제로 도커를 설치하고 컨테이너를 실행해 보도록 하겠습니다.";
+//        String text = "이 글은 도커에 대해 1도 모르는 시스템 관리자나 서버 개발자를 대상으로 도커 전반에 대해";
+//        autoNewlineProc(text);
 
-        System.out.println("test3.length() = " + test3.length());
-        System.out.println("test4.length() = " + test4.length());
+        // Pattern.matches("^[\uac00-\ud7a3]*$", temp);
+        // 아스키 33~126
+        // 65~90 대문자 || 97~122 소문자
+        String patternUpper = "(6([5-9]))|([7-8]([0-9]))|(9([0-1]))";
+        String patternLower = "(9([7-9]))|(10([0-9]))|(11([0-9]))|(12([0-2]))";
+        String patternASCII = "(3([3-9]))|([4-9]([0-9]))|(10([0-9]))|(11([0-9]))|(12([0-6]))";
 
-//        byte[] test33 = test3.getBytes(StandardCharsets.UTF_8);
-        byte[] test33 = test3.getBytes(StandardCharsets.US_ASCII);
-//        byte[] test44 = test4.getBytes(StandardCharsets.UTF_8);
-        byte[] test44 = test4.getBytes(StandardCharsets.US_ASCII);
+        String temp9 = autoNewlineProc.autoNewlineProc(text);
+        System.out.println("temp = \n" + temp9);
 
-        System.out.println("test33 = " + test33);
-        System.out.println("test44 = " + test44);
-
-        String test333 = new String(test33, StandardCharsets.UTF_8);
-        String test444 = new String(test44, StandardCharsets.UTF_8);
-
-        System.out.println("test333 = " + test333);
-        System.out.println("test444 = " + test444);
-        
-
+        ///////////////////////////////////////////////////////////////////////////////////////
         long endTime = System.currentTimeMillis();
         System.out.println("start Time = " + startTime);
         System.out.println("end Time = " + endTime);
@@ -84,10 +74,5 @@ public class PurpleApplication {
         System.out.println("proc Time = " + (endTime - startTime));
     }
 
-
-    public static String autoNewlineProc(String text){
-        System.out.println("text = " + text.length());
-        return "";
-    }
 
 }
---
+++ src/main/java/com/purple/autoNewlineProc.java
@@ -0,0 +1,86 @@
+package com.purple;
+
+public class autoNewlineProc {
+
+    public static String autoNewlineProc(String text) {
+        String patternASCII = "(3([3-9]))|([4-9]([0-9]))|(10([0-9]))|(11([0-9]))|(12([0-6]))";
+        String resultString = "";
+        boolean endKorean = false;
+        int index = 0;
+
+
+//        인덱스가 하나 필요함 -> 인덱스로 안돌아가니까 위치가 바뀌면 못따라감
+        try {
+            byte[] temp = text.getBytes("euc-kr");
+            System.out.println("temp.length = " + temp.length);
+
+            double tempLength = temp.length;
+            int loop = (int) Math.ceil(((tempLength/80)*100)/100);
+
+            for(int i = 0; i<loop; i++){
+                if(tempLength < i*80 + 80){
+
+                }
+                else if(i != 0){
+                    String checkASCII = String.valueOf(temp[index]);
+                    if(!checkASCII.matches(patternASCII)){
+                        endKorean = true;
+                        index = index-1;
+                    }
+                    resultString += getString(text, index, 80, endKorean);
+                }
+                else if (i == 0){
+                    resultString += getString(text, index, 80, endKorean);
+                }
+                index = index + 80;
+                resultString += "\n";
+                endKorean = false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultString;
+    }
+
+    public static String getString(String str, int sPoint, int length, boolean endKorean) throws Exception{
+        String patternASCII = "(3([3-9]))|([4-9]([0-9]))|(10([0-9]))|(11([0-9]))|(12([0-6]))";
+        String CharSet = "euc-kr";
+        byte[] bytes = str.getBytes(CharSet);
+        byte[] value = new byte[length];
+
+//        String koreanCheck = String.valueOf(bytes[sPoint]);
+        // First letter >> skip space
+        int spaceCheck = 0;
+//        int spaceCheck = (koreanCheck.equals("32")) ? 1 : 0;
+//        if(koreanCheck.matches(patternASCII)){
+//            value[length] = bytes[sPoint + length];
+//        }
+
+//        if(endKorean){
+//            sPoint = sPoint-1;
+//        }
+        // less
+        if(bytes.length < sPoint + length){
+            int less = bytes.length-sPoint;
+            for(int i = spaceCheck; i < less; i++){
+                value[i] = bytes[sPoint + i];
+            }
+        }
+        // max
+        else{
+            for(int i = spaceCheck; i < length-1; i++){
+                value[i] = bytes[sPoint + i];
+            }
+//            String temp = String.valueOf(bytes[length]);
+//            if(temp.matches(patternASCII)){
+//                value[length] = bytes[sPoint + length];
+//            }
+//            else{
+//            }
+        }
+        System.out.println("sPoint = " + sPoint);
+        return new String(value, CharSet).trim();
+    }
+
+
+}
