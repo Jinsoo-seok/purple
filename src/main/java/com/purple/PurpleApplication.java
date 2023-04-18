15a15810c9fc43d3b8935e6ecc4c3303122baecd
SJS <jinsoo1932@gmail.com>
2023-04-18 09:19:42 +0900

    5번

--- src/main/java/com/purple/PurpleApplication.java
+++ src/main/java/com/purple/PurpleApplication.java
@@ -52,19 +52,23 @@ public class PurpleApplication {
 
         ///////////////////////////////////////////////////////////////////////////////////////
         // 4. 한글 체크, 공백 체크 필요
-        String text = "이 글은 도커에 대해 1도 모르는 시스템 관리자나 서버 개발자를 대상으로 도커 전반에 대해 얕고 넓은 지식을 담고 있습니다. 도커가 등장한 배경과 도커의 역사, 그리고 도커의 핵심 개념인 컨테이너와 이미지에 대해 알아보고 실제로 도커를 설치하고 컨테이너를 실행해 보도록 하겠습니다.";
+//        String text = "이 글은 도커에 대해 1도 모르는 시스템 관리자나 서버 개발자를 대상으로 도커 전반에 대해 얕고 넓은 지식을 담고 있습니다. 도커가 등장한 배경과 도커의 역사, 그리고 도커의 핵심 개념인 컨테이너와 이미지에 대해 알아보고 실제로 도커를 설치하고 컨테이너를 실행해 보도록 하겠습니다.";
 //        String text = "이 글은 도커에 대해 1도 모르는 시스템 관리자나 서버 개발자를 대상으로 도커 전반에 대해";
-//        autoNewlineProc(text);
-
-        // Pattern.matches("^[\uac00-\ud7a3]*$", temp);
         // 아스키 33~126
         // 65~90 대문자 || 97~122 소문자
-        String patternUpper = "(6([5-9]))|([7-8]([0-9]))|(9([0-1]))";
-        String patternLower = "(9([7-9]))|(10([0-9]))|(11([0-9]))|(12([0-2]))";
-        String patternASCII = "(3([3-9]))|([4-9]([0-9]))|(10([0-9]))|(11([0-9]))|(12([0-6]))";
+//        String patternUpper = "(6([5-9]))|([7-8]([0-9]))|(9([0-1]))";
+//        String patternLower = "(9([7-9]))|(10([0-9]))|(11([0-9]))|(12([0-2]))";
+//        String patternASCII = "(3([3-9]))|([4-9]([0-9]))|(10([0-9]))|(11([0-9]))|(12([0-6]))";
+//
+//        String temp9 = autoNewlineProc.autoNewlineProc(text);
+//        System.out.println("temp = \n" + temp9);
 
-        String temp9 = autoNewlineProc.autoNewlineProc(text);
-        System.out.println("temp = \n" + temp9);
+        ///////////////////////////////////////////////////////////////////////////////////////
+        // 5. 좀 이상한듯?
+        int num = 98;
+
+        System.out.println("plusMinus = " + plusMinus.plusMinus(num));
+
 
         ///////////////////////////////////////////////////////////////////////////////////////
         long endTime = System.currentTimeMillis();
@@ -76,3 +80,4 @@ public class PurpleApplication {
 
 
 }
+
---
+++ src/main/java/com/purple/plusMinus.java
@@ -0,0 +1,41 @@
+package com.purple; 
+public class plusMinus {
+
+    public static String plusMinus(int number){
+        // 9의 배수(apple) 말고는 패턴이...
+        String[] fruitList = {
+                "kiwi",      "pear",      "kiwi",      "banana",    "melon",     "banana",    "melon",     "pineapple", "apple",
+                "pineapple", "cucumber",  "pineapple", "cucumber",  "orange",    "grape",     "orange",    "grape",     "apple",
+                "grape",     "cherry",    "pear",      "cherry",    "pear",      "kiwi",      "banana",    "kiwi",      "apple",
+                "melon",     "banana",    "melon",     "pineapple", "melon",     "pineapple", "cucumber",  "orange",    "apple",
+                "orange",    "grape",     "orange",    "grape",     "cherry",    "pear",      "cherry",    "pear",      "apple",
+                "pear",      "kiwi",      "banana",    "kiwi",      "banana",    "melon",     "pineapple", "melon",     "apple",
+                "cucumber",  "pineapple", "cucumber",  "orange",    "cucumber",  "orange",    "grape",     "cherry",    "apple",
+                "cherry",    "pear",      "cherry",    "pear",      "kiwi",      "pear",      "kiwi",      "banana",    "apple",
+                "banana",    "melon",     "pineapple", "melon",     "pineapple", "cucumber",  "pineapple", "cucumber",  "apple",
+                "grape",     "orange",    "grape",     "cherry",    "grape",     "cherry",    "pear",      "cherry",    "apple",
+                "kiwi",      "banana",    "kiwi",      "banana",    "melon",     "banana",    "melon",     "pineapple", "apple",
+                "pineapple"
+        };
+
+        int sum = 0;
+        int tempNumber = number;
+
+        // 101 ~ 10000 >> 99로 수렴
+//        if(tempNumber > 100){
+//            return "apple";
+//        }
+//        else {
+            while (tempNumber > 100) {
+                String numStr = Integer.toString(tempNumber);
+                sum = 0;
+                for (int i = 0; i < numStr.length(); i++) {
+                    sum += Integer.parseInt(Character.toString(numStr.charAt(i)));
+                }
+                tempNumber = tempNumber - sum;
+            }
+//        }
+
+        return fruitList[tempNumber];
+    }
+}
